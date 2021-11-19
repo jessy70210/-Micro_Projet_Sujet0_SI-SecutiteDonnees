e642042855d0eb7eb29cd42760e0584d0a00ac7e
Jessy LEBRUN <jessy70210@gmail.com>
2021-11-19 11:50:30 +0100

    Mise a jour des fichier externe

--- lib/file.php
+++ lib/file.php
@@ -2,7 +2,13 @@
 class file{
   public static function build_path($path_array){
     $d_s = DIRECTORY_SEPARATOR;
-    $ROOT_FOLDER = __DIR__ . "/..";
+    $ROOT_FOLDER = __DIR__ . "\\..";
     return $ROOT_FOLDER . $d_s . join($d_s, $path_array);
+
+  }
+
+  public static  function file_path ($path_array){
+      //return DIRECTORY_SEPARATOR . join($path_array);
+      return  __dir__;
   }
 }
--- view/navbar.php
+++ view/navbar.php
@@ -4,9 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php if(!isset($y)){ echo $pageTitle;} ?></title>
-        <link rel="stylesheet" href=""> <!-- Inclure ici votre css -->
-        <script src=""></script> <!-- Inclure ici votre js -->
+        <!-- <link rel="stylesheet" href="<?php //require((File::file_path(array("resources\\css", "style.css")))); ?>"> --> <!-- Inclure ici votre css -->
+
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> <!-- Inclure ici votre favicon -->
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+        <link rel="stylesheet" href="../../resources/css/style.css">
+        <!-- Font Awesome -->
+        <link href="../../resources/css/all.min.css" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+        <!-- MDB -->
+        <link href="../../resources/css/mdb.min.css" rel="stylesheet"/>
+
+        <!-- MDB -->
+        <script src="../../resources/script/script.js"></script>
     </head>
     <body>
