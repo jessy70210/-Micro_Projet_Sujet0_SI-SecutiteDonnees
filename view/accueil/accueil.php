f73c4a1cccf2ae7040279caa83f1b6428b060c22
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2021-11-21 12:09:36 +0100

    1er version de la page d'accueil

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,4 +1,28 @@
-<?php
-
-echo "<h1>Test</h1>";
-
+<div class="container h-100 d-flex flex-column justify-content-around">
+    <div class="row m-5">
+        <div class="col-6 offset-3 ">
+            <h1 class="text-white font-pacifico text-center">Restaurant</h1>
+            <h3 class="text-white-50 font-pacifico text-center">à la bonne heure</h3>
+        </div>
+    </div>
+    <div class="row m-5">
+        <p class="text-white text-center fs-1">Bienvenu sur notre application<br>Grâce à notre service de menu diginal vous pouvez consulté, commandé le menu et surtout profité.<br><br>Il est l'heure de passer à table</p>
+    </div>
+    <div class="row- fixed-bottom m-5">
+        <div class="col-4 text-center">
+            <button type="button" class="btn btn-outline-light">
+                Passer commande une
+            </button>
+        </div>
+        <div class="col-4 text-center">
+            <button type="button" class="btn btn-outline-light">
+                Voir le menu
+            </button>
+        </div>
+        <div class="col-4 text-center">
+            <button type="button" class="btn btn-outline-light">
+                Rejoindre une commande
+            </button>
+        </div>
+    </div>
+</div>
--- view/navbar.php
+++ view/navbar.php
@@ -24,74 +24,66 @@
     <body>
 
     <header>
-
-
-
-        <div class="bg-image vh-40 shadow-1-strong">
-        <!-- Background image -->
 <!---->
-<!--            <video style="min-width: 100%; min-height: 100%;" playsinline autoplay muted loop>-->
-<!--                <source class="h-100"-->
-<!--                        src="https://mdbootstrap.com/img/video/animation-intro-min.mp4"-->
-<!--                        type="video/mp4"-->
-<!--                />-->
-<!--            </video>-->
-
-            <img style="min-width: 100%; min-height: 100%;" src="../../resources/img/resto.jpg" alt="resto">
-            <div class="mask" style="
-            background: linear-gradient(
-              45deg,
-              rgba(236,77,29,0.3),
-              rgba(214,14,14,0.3) 100%
-            );
-          ">
-                <div>
-
-                    <nav class="navbar navbar-expand-lg bg-black" >
-                        <div class="container-fluid">
-                            <a class="navbar-brand text-white" href="#">Nom</a>
-
+<!---->
+<!---->
+<!--        <div class="bg-image vh-100 shadow-1-strong">-->
+<!--            <img style="min-width: 100%; min-height: 100%;" src="../../resources/img/resto.jpg" alt="resto">-->
+<!--            <div class="mask" style="-->
+<!--            background: linear-gradient(-->
+<!--              45deg,-->
+<!--              rgba(236,77,29,0.3),-->
+<!--              rgba(214,14,14,0.3) 100%-->
+<!--            );-->
+<!--          ">-->
+<!--                <div>-->
+<!---->
+<!--                    <nav class="navbar navbar-expand-lg bg-black" >-->
+<!--                        <div class="container-fluid">-->
+<!--                            <a class="navbar-brand text-white" href="#">Nom</a>-->
+<!---->
                             <!-- Toggle button -->
-                            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <i class="fas fa-bars"></i>
-                            </button>
-
+<!--                            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                                <i class="fas fa-bars"></i>-->
+<!--                            </button>-->
+<!---->
                             <!-- Collapsible wrapper -->
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                                </ul>
-
+<!--                            <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
+<!---->
+<!--                                </ul>-->
+<!---->
                                 <!-- Icons -->
-                                <ul class="navbar-nav d-flex flex-row me-1">
-                                    <li class="nav-item me-3 me-lg-0">
-                                        <a class="nav-link text-white" href="#"><i class="fas fa-language"></i></a>
-                                    </li>
-                                    <li class="nav-item me-3 me-lg-0">
-                                        <a class="nav-link text-white" href="#"><i class="fab fa-yelp"></i></a>
-                                    </li>
-                                </ul>
-
-                            </div>
-                        </div>
+<!--                                <ul class="navbar-nav d-flex flex-row me-1">-->
+<!--                                    <li class="nav-item me-3 me-lg-0">-->
+<!--                                        <a class="nav-link text-white" href="#"><i class="fas fa-language"></i></a>-->
+<!--                                    </li>-->
+<!--                                    <li class="nav-item me-3 me-lg-0">-->
+<!--                                        <a class="nav-link text-white" href="#"><i class="fab fa-yelp"></i></a>-->
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!---->
+<!--                            </div>-->
+<!--                        </div>-->
                         <!-- Container wrapper -->
-                    </nav>
+<!--                    </nav>-->
                     <!-- Navbar -->
-
-                </div>
-                <div class="container d-flex align-items-center justify-content-center text-center h-100">
-                    <div class="text-white">
-                        <h1 class="mb-3"><?php if(!isset($y)){ echo $pageTitle;} ?></h1>
-<!--                        <h5 class="mb-4">Best & free guide of responsive web design</h5>-->
-                        <a class="btn btn-outline-light btn-lg m-2" href="#" role="button"
-                           rel="nofollow" target="_blank">Regarder le menu<br>et commandé</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="#" target="_blank"
-                           role="button">Rejoindre une<br>addistion existente</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!---->
+<!--                </div>-->
+<!--                <div class="container d-flex align-items-center justify-content-center text-center h-100">-->
+<!--                    <div class="text-white">-->
+<!--                        <h1 class="mb-3">--><?php //if(!isset($y)){ echo $pageTitle;} ?><!--</h1>-->
+<!--                        <a class="btn btn-outline-light btn-lg m-2" href="#" role="button"-->
+<!--                           rel="nofollow" target="_blank">Regarder le menu<br>et commandé</a>-->
+<!--                        <a class="btn btn-outline-light btn-lg m-2" href="#" target="_blank"-->
+<!--                           role="button">Rejoindre une<br>addistion existente</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
         <!-- Background image -->
     </header>
+    <main>
+
 
 
